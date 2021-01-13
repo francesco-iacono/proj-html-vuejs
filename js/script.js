dbacd6ebb06eeba357efa9d6c62ee1494d7c2f85
francesco <f.iacono89@gmail.com>
2021-01-13 10:51:16 +0100

    inizio 4 section

--- js/script.js
+++ js/script.js
@@ -53,6 +53,26 @@ var app = new Vue({
         students: '76 Students',
         img: 'img/stock-full-hd-05-480x298.jpg'
       }
+    ],
+    tutorings: [
+      {
+        img: 'img/home-2-image-box-01.png',
+        name: 'Customize Preferences',
+        description: 'Learners are given the right to arrange and customize their study schedule and timetable based on preferences.',
+        link: 'Start here'
+      },
+      {
+        img: 'img/about-us-02-image-box-02.png',
+        name: 'Cloud Storage',
+        description: 'You can store online lessons via online cloud or download to your device and revise lessons on the way.',
+        link: 'Get Free Quote'
+      },
+      {
+        img: 'img/home-2-image-box-03.png',
+        name: 'Multimedia Channels',
+        description: 'We provide study materials with various formats: video, audio, slides, doc, prints, books and applications.',
+        link: 'Start now'
+      }
     ]
   }
 })
